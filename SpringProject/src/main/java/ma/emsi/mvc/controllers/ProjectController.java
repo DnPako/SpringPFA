285bad077070b8db91d2c84253c439afe4f0fded
Mouad MASTOUR <mouadmastour@gmail.com>
2016-01-03 21:20:22 +0000

    Controllers second modification

--- SpringProject/src/main/java/ma/emsi/mvc/controllers/ProjectController.java
+++ SpringProject/src/main/java/ma/emsi/mvc/controllers/ProjectController.java
@@ -1,13 +1,16 @@
 package ma.emsi.mvc.controllers;
 
-
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import ma.emsi.mvc.data.entities.Project;
 import ma.emsi.mvc.data.services.ProjectService;
 
 @Controller
@@ -17,21 +20,29 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 
-	@RequestMapping(value="/find")
-	public String find(Model model){
-		model.addAttribute("projects",this.projectService.findAll());
+	@RequestMapping(value = "/{projectId}")
+	public String findProject(Model model, @PathVariable("projectId") Long projectId) {
+		model.addAttribute("project", this.projectService.find(projectId));
+		return "project";
+	}
+
+	@RequestMapping(value = "/find")
+	public String find(Model model) {
+		model.addAttribute("projects", this.projectService.findAll());
 		return "projects";
 	}
-	
-	@RequestMapping(value="/add",method=RequestMethod.GET)
-	public String addProject(){
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String addProject(HttpSession session) {
+		session.setAttribute("token", "12345");
 		System.out.println("invoking addProject");
 		return "project_add";
 	}
 
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public String saveProject(){
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String saveProject(@ModelAttribute Project project) {
 		System.out.println("invoking saveProject");
+		System.out.println(project);
 		return "project_add";
 	}
 }
