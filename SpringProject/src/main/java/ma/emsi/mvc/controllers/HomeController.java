1fecb59fed581a8467498466fbcf9c94e543c5fa
Mouad MASTOUR <mouadmastour@gmail.com>
2016-01-03 18:01:21 +0000

    Learning Controllers

--- SpringProject/src/main/java/ma/emsi/mvc/controllers/HomeController.java
+++ SpringProject/src/main/java/ma/emsi/mvc/controllers/HomeController.java
@@ -1,13 +1,23 @@
 package ma.emsi.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ma.emsi.mvc.data.entities.Project;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
-	public String goHome() {
+	@RequestMapping("/")
+	public String goHome(Model model) {
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by NASA");
+		
+		model.addAttribute("currentProject", project);
+		
 		return "home";
 	}
 
